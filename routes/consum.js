f16d767b71313ffb4541465fa7447784eaf18088
elenapolyakova <65355090+elenapolyakova@users.noreply.github.com>
2020-06-24 11:50:29 +0300

    consum

--- routes/consum.js
+++ routes/consum.js
@@ -8,8 +8,8 @@ const toFloat = val => {
 
   const insConsum = function(request, response, next){
       let consumData = request.body.consumData;
-      db.query(`INSERT INTO Consum (DT, I_A, I_B, I_C, V_A, V_B, V_C, id_eq_equipment, id_reg)
-        VALUES ($1::DATETIME, $2::FLOAT, $3::FLOAT, $4::FLOAT, $5::FLOAT, $6::FLOAT, $7::FLOAT, $8::INT, $9::INT)
+      db.query(`INSERT INTO Consum (DT, I_A, I_B, I_C, V_A, V_B, V_C, id_eq_equipment, id_reg_reg)
+        VALUES ($1::TIMESTAMP, $2::FLOAT, $3::FLOAT, $4::FLOAT, $5::FLOAT, $6::FLOAT, $7::FLOAT, $8::INT, $9::INT)
         RETURNING id_cons`, [ 
             consumData.dt !=='' ? new Date(consumData.dt) : null,
             toFloat(consumData.I_A),
